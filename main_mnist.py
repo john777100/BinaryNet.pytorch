6975b1a2e9b412469492fac83733c109cb9a6886
Bo-Ray Yeh <30313762+jared12222@users.noreply.github.com>
2021-10-22 21:12:25 -0400

    Change test_batch_size
    Change to 100 to avoid overload GPU

--- main_mnist.py
+++ main_mnist.py
@@ -16,7 +16,7 @@ from multiprocessing import freeze_support
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 256)')
-parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+parser.add_argument('--test-batch-size', type=int, default=100, metavar='N',
                     help='input batch size for testing (default: 1000)')
 parser.add_argument('--epochs', type=int, default=100, metavar='N',
                     help='number of epochs to train (default: 10)')
