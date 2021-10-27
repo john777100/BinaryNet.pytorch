61674ebaa087c1271c474ab3580e511937f6c6cb
ethan12377 <ethan@LAPTOP-2KFTTVL8.localdomain>
2021-10-27 02:09:32 -0400

    test

--- models/binarized_modules.py
+++ models/binarized_modules.py
@@ -97,8 +97,8 @@ class myBinarizeLinear(nn.Linear):
         x_num = x_num.expand(256, batch_size, input_size).permute(1, 2, 0) # x_num.shape = [64, 784, 256]
 
         #print(x_num.get_device())
-        x_num = x_num.to('cuda:3')
-        bin_input = bin_input.to('cuda:3')
+        #x_num = x_num.to('cuda:3')
+        #bin_input = bin_input.to('cuda:3')
         bin_input = bin_input.less(x_num)
         bin_input = bin_input.int().float().multiply(2).add(-1)# * 2 - 1
         #torch.set_printoptions(threshold=100000000000000)
