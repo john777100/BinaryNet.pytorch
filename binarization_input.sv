703e13a9e3cdc4c77f8f901578a970d406185807
Cheng-Lin Lee <iamjohn@caen-vnc-mi03.engin.umich.edu>
2021-10-29 01:04:35 -0400

    finish testing binarization_input.sv

--- binarization_input.sv
+++ binarization_input.sv
@@ -1,13 +1,12 @@
 module binarization_input(
-	input	[`KERNEL_SIZE-1:0][`BIT_WIDTH-1:0]		pixel_in,
-	output	[`KERNEL_SIZE-1:0][`CHANNEL_CNT-1:0]	pixel_out
+	input			[`KERNEL_SIZE-1:0][`BIT_WIDTH-1:0]		pixel_in,
+	output logic	[`KERNEL_SIZE-1:0][`CHANNEL_CNT-1:0]	pixel_out
 );
 	logic	[`BIT_WIDTH:0] 							negative_cnt;	// range 1 - 256 mapping to 0 - 255
-	input	[`KERNEL_SIZE-1:0][`BIT_WIDTH-1:0]		pixel_in,
 	always_comb begin
-		pixel_out		= 'b1;
+		pixel_out		= 'b0 - 1;
 		for(int i = 0; i<`KERNEL_SIZE; i++) begin
-			negative_cnt	= ((`BIT_WIDTH)'d128 - {pixel_in[i][`BIT_WIDTH-1],pixel_in[i]}); // (256 - pixel * 2) >> 1 === 128 - pixel
+			negative_cnt	= (128 - {pixel_in[i][`BIT_WIDTH-1],pixel_in[i]}); // (256 - pixel * 2) >> 1 === 128 - pixel
 			pixel_out[i] = pixel_out[i] >> negative_cnt;
 		end
 	end
--- testbench_BI.sv
+++ testbench_BI.sv
@@ -1,19 +1,24 @@
 module testbench_BI();
 
-	binarization_input(
-		.pixel_in(pixel_in),
-		.pixel_out(pixel_out)
-	);
 	
 	logic [`KERNEL_SIZE-1:0][`BIT_WIDTH-1:0]		pixel_in;
 	logic [`KERNEL_SIZE-1:0][`CHANNEL_CNT-1:0]		pixel_out;
 
+	binarization_input b0(
+		.pixel_in(pixel_in),
+		.pixel_out(pixel_out)
+	);
 	logic [`CHANNEL_CNT-1:0]						temp;
 	
 	initial begin
-		pixel_in[0]	 	= -8'd128 ;
+		temp			= 'b0 - 1;
+		pixel_in[0]	 	= 8'd127 ;
+		
 		#1
-		if(pixel_out[0] != temp >> 128) $display("Fail!, output: %h, golden: %h",pixel_out[0],temp >> 128)
+		$display("output: %h, golden: %h",pixel_out[0],temp >> 128);
+		
+		if(pixel_out[0] != temp >> 1) $display("Fail!, output: %h, golden: %h",pixel_out[0],temp >> 1);
+		else $display("PASS!!!!!!");
 	end
 
 endmodule
